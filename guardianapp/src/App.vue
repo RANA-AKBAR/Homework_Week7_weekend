2a239c824ef3ac1f075aef7fd0a994e58a8bc8c7
Rana-akbar <rana.akbar.08@gmail.com>
2019-08-09 14:06:02 +0100

    -server set up

---
+++ guardianapp/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  },
+  mounted() {
+    fetch('https://content.guardianapis.com/search?q=brexit&format=json&api-key=test')
+    .then(response => response.json())
+    .then(console.log())
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
